7491bd057a682878bc27c954f74c5bc3b76fc0e3
brianpompey <brianthescholar@gmail.com>
2021-12-06 16:16:39 -0500

    inline styling

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text changed!')}/>
+    <View style={{padding : 30}}>
+      <View>
+        <TextInput placeholder="Course Goal"/>
+        <Button title="ADD"/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
