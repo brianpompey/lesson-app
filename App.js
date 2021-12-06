def55b15ee8823edbd66958fb8f7dfa393c36d8a
brianpompey <brianthescholar@gmail.com>
2021-12-06 16:47:10 -0500

    list sytiling items

--- App.js
+++ App.js
@@ -2,17 +2,29 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  }
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput 
           placeholder="Course Goal"
           style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="ADD"/>
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
       <View>
-
+        {courseGoals.map((goal) => <View style={styles.listItem}><Text key={goal}>{goal}</Text></View>)}
       </View>
     </View>
   );
@@ -32,5 +44,12 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 })
